d337369a9af52814a2803f1e7ce9fc1a5ffbee8c
nobe4 <haffreinguev@gmail.com>
2020-10-29 00:32:51 +0100

    Make it easier to save config

--- js/exercises/intervals.js
+++ js/exercises/intervals.js
@@ -11,6 +11,33 @@ Vue.createApp({
       selectedIntervals: []
     }
   },
+  computed: {
+    selectedIntervalsValue: {
+      get () {
+        // Add each found selected intervals index in a single binary number.
+        const value = this.selectedIntervals
+	  .reduce((a, b) => a + 1 << b, 0)
+        return value.toString(20)
+      },
+      set (value) {
+        this.selectedIntervals = []
+
+        if (!value) return
+
+        // Convert to a binary string
+        const selection = parseInt(value, 20)
+	  .toString(2)
+	  .split('')
+	  .reverse()
+
+        for (var i in selection) {
+	  if (selection[i] === '1') {
+	    this.selectedIntervals.push(i)
+	  }
+        }
+      }
+    }
+  },
   methods: {
     reload () {
       this.interval = Interval.random(null, this.selectedIntervals)
